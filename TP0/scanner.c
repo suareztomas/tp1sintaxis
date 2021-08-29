a5359b1d233527763d04ad241f4076296ad30839
unknown <tsuarez@frba.utn.edu.ar>
2021-08-29 19:54:15 -0300

    Arreglo en tokens

--- TP0/scanner.c
+++ TP0/scanner.c
@@ -3,6 +3,7 @@
 #include "scanner.h"
 
 void get_token(){
+    token tokenType;
     char str[50];
     int i = 0;
     char c;
@@ -10,9 +11,11 @@ void get_token(){
         // SEPARADOR
         if (c == ',') {
             printf("Separador: , \n");
+            tokenType = SEP;
         // FDT
         } else if (c == EOF) {
             printf("Fin de texto: \n");
+            tokenType = FDT;
             break;
         // ESPACIO (ignorar)
         } else if (isspace(c)){
@@ -24,6 +27,7 @@ void get_token(){
             char cAux = getchar();
             // Me fijo si lo que sigue es un caracter que me corta el string
             if (cAux == EOF || cAux == ',' || isspace(cAux)) {
+                tokenType = CAD;
                 str[i] ='\0';
                 printf("Cadena: %s", str);
                 printf("\n");
